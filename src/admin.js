01515024849a05244e07ce8d9e310edf1d948417
DarTam132 <darius.tamasan96@gmail.com>
2023-04-25 14:36:24 +0300

    admin rendering movies

--- src/admin.js
+++ src/admin.js
@@ -1,47 +1,41 @@
 import { MOVIE_DB_URL, types, MOCK_API_LINK } from "./config.js";
+let moviesTable = document.querySelector(".table-content");
 
-const getMovies = async (type) => {
-  try {
-    let movies = [];
-
-    const genreId = types[type];
-    if (!genreId) {
-      throw new Error(`Invalid genre type: ${type}`);
-    }
-
-    const response = await fetch(`${MOVIE_DB_URL}${genreId}`);
-    if (!response.ok)
-      throw new Error(`Failed to fetch movies from page ${page}`);
-
-    const data = await response.json();
-    movies.push(...data.results);
-    console.log(movies);
-    movies.forEach(async (movie) => {
-      const customData = {
-        movie_title: movie.original_title,
-        description: movie.overview,
-        language: movie.original_language,
-        movie_photo: movie.poster_path,
-        price: Math.floor(Math.random() * 50),
-        type: type,
-      };
-
-      const mockApi = await fetch(MOCK_API_LINK, {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(customData),
-      });
-
-      const customResponse = await mockApi.json();
-      console.log(customResponse);
-    });
-
-    return movies;
-  } catch (error) {
-    console.error(error);
-  }
+const mockAPImovies = async () => {
+  const res = await fetch(MOCK_API_LINK);
+  const mockApi = await res.json();
+  mockApi.forEach((movie) => {
+    moviesTable.innerHTML += ` <div class="movie-line ${
+      movie.id % 2 === 1 ? "background-odd" : "background-even"
+    }">
+    <div class="nr-title-img">
+      <span class="movie-number table-style">${movie.id}.</span>
+      <img
+        class="movie-photo"
+        src="https://image.tmdb.org/t/p/w500${mockApi[movie.id].movie_photo}"
+        alt=""
+      />
+      <span class="movie-title table-style"
+        >${movie.movie_title}</span
+      >
+    </div>
+    <div class="price-btns"> 
+    <span class="movie-price table-style">${movie.price}$</span>
+    <button class="change-btn table-style">Change price</button>
+    <button class="delete-btn table-style">Delete</button>
+    </div>
+  </div>`;
+  });
+  console.log(moviesTable);
+  // const imgElement = document.createElement("img"); // Create img element
+  // imgElement.src = `https://image.tmdb.org/t/p/w500${mockApi[9].movie_photo}`; // Set src attribute to movie image URL
+  // document.body.appendChild(imgElement);
+  console.log(mockApi);
 };
-// getMovies("action");
-// getMovies("love");
-getMovies("animated");
-// getMovies("horror");
+
+mockAPImovies();
+//  movies.forEach((movie) => {
+//     const imgElement = document.createElement("img"); // Create img element
+//     imgElement.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`; // Set src attribute to movie image URL
+//     document.body.appendChild(imgElement); // Append img element to the body or any other desired location in the HTML document
+//   });
--- src/config.js
+++ src/config.js
@@ -8,3 +8,52 @@ export const types = {
 };
 export const MOCK_API_LINK =
   "https://644285cb33997d3ef912811f.mockapi.io/admin/movies";
+// export const MOVIE_IMG_LINK = ``
+
+// Population the mock API with data from the movie DB API
+
+const getMovies = async (type) => {
+  try {
+    let movies = [];
+
+    const genreId = types[type];
+    if (!genreId) {
+      throw new Error(`Invalid genre type: ${type}`);
+    }
+
+    const response = await fetch(`${MOVIE_DB_URL}${genreId}`);
+    if (!response.ok)
+      throw new Error(`Failed to fetch movies from page ${page}`);
+
+    const data = await response.json();
+    movies.push(...data.results);
+    console.log(movies);
+    movies.forEach(async (movie) => {
+      const customData = {
+        movie_title: movie.original_title,
+        description: movie.overview,
+        language: movie.original_language,
+        movie_photo: movie.poster_path,
+        price: Math.floor(Math.random() * 50),
+        type: type,
+      };
+
+      const mockApi = await fetch(MOCK_API_LINK, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(customData),
+      });
+
+      const customResponse = await mockApi.json();
+      console.log(customResponse);
+    });
+
+    return movies;
+  } catch (error) {
+    console.error(error);
+  }
+};
+// getMovies("action");
+// getMovies("love");
+// getMovies("animated");
+// getMovies("horror");
